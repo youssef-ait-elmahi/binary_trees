b484ac65e013df88caaeb44c93342a0ae7529155
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-08-31 19:31:36 +0100

    header file edited

--- binary_trees.h
+++ binary_trees.h
@@ -47,6 +47,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-size_t binary_tree_height(const binary_tree_t *tree);
-
 #endif /* BINARY_TREE_H */
