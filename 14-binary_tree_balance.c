580bb25b95c57c0956fcd51b59a666e5ea195afb
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-08-31 19:28:16 +0100

    edit task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,19 +7,19 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int height_left = 0;
-    int height_right = 0;
+	int height_left = 0;
+	int height_right = 0;
 
-    /* Check if tree is NULL */
-    if (tree == NULL)
-        return (0);
+	/* Check if tree is NULL */
+	if (tree == NULL)
+		return (0);
 
-    /* Measure the height of the left and right subtrees */
-    height_left = (int)binary_tree_height(tree->left);
-    height_right = (int)binary_tree_height(tree->right);
+	/* Measure the height of the left and right subtrees */
+	height_left = (int)binary_tree_height(tree->left);
+	height_right = (int)binary_tree_height(tree->right);
 
-    /* Return the difference between the two heights */
-    return (height_left - height_right);
+	/* Return the difference between the two heights */
+	return (height_left - height_right);
 }
 
 /**
@@ -30,19 +30,19 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height_left = 0;
-    size_t height_right = 0;
+	size_t height_left = 0;
+	size_t height_right = 0;
 
-    /* Check if tree is NULL */
-    if (tree == NULL)
-        return (0);
+	/* Check if tree is NULL */
+	if (tree == NULL)
+		return (0);
 
-    /* Recursively measure the height of the left and right subtrees */
-    if (tree->left != NULL)
-        height_left = 1 + binary_tree_height(tree->left);
-    if (tree->right != NULL)
-        height_right = 1 + binary_tree_height(tree->right);
+	/* Recursively measure the height of the left and right subtrees */
+	if (tree->left != NULL)
+		height_left = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		height_right = 1 + binary_tree_height(tree->right);
 
-    /* Return the greater of the two heights */
-    return ((height_left > height_right) ? height_left : height_right);
+	/* Return the greater of the two heights */
+	return ((height_left > height_right) ? height_left : height_right);
 }
